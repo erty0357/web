30326cd72c63ad0044d2b728bf825754c25f1a41
erty0357 <erty0357@naver.com>
2025-06-02 02:07:04 +0900

    Update WebConfig.java

--- src/main/java/backend/config/WebConfig.java
+++ src/main/java/backend/config/WebConfig.java
@@ -12,8 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // 모든 엔드포인트
                 .allowedOrigins(
                         "http://localhost:5173",              // 로컬 개발 환경
-                        "https://web-vx1u.onrender.com",      // 백엔드 배포 서버
-                        "https://yuclub-mxyqm30vm-kim-sang-hyuns-projects.vercel.app" // ✅ 프론트 Vercel 주소 명시적으로 추가
+                        "https://web-vx1u.onrender.com", // 백엔드 배포 서버
+                        "https://yuclub-git-main-kim-sang-hyuns-projects.vercel.app"
+                        "https://yuclub-fc6vf11ge-kim-sang-hyuns-projects.vercel.app" // ✅ 프론트 Vercel 주소 명시적으로 추가
                 )
                 .allowedMethods("*") // GET, POST, PUT, DELETE 등 모두 허용
                 .allowedHeaders("*"); // 모든 헤더 허용
